762401c397ee8699f809e0c077e21ea1b28787a5
Sergey Khoroshavin <skhoroshavin@gmail.com>
2022-12-26 13:05:25 +0100

    Remove majority of legacy code from writer

--- internal/writer/old_write.go
+++ internal/writer/old_write.go
@@ -1,27 +1,51 @@
 package writer
 
 import (
+	"fmt"
+	"github.com/skhoroshavin/automap/internal/mapper"
+	"github.com/skhoroshavin/automap/internal/mapper/ast"
 	"github.com/skhoroshavin/automap/internal/oldmapper"
 	"io"
 )
 
-func OldWrite(out io.Writer, reg *oldmapper.Registry) (err error) {
-	err = writeOldHeader(out, reg.Package())
-	if err != nil {
-		return
+func OldWrite(out io.Writer, reg *oldmapper.Registry) error {
+	pkg := &ast.Package{
+		Name:    reg.Package(),
+		Imports: reg.Imports(),
+		Mappers: make([]*ast.Mapper, len(reg.Mappers())),
 	}
 
-	err = writeImports(out, reg.Imports())
-	if err != nil {
-		return
-	}
-
-	for _, mapper := range reg.Mappers() {
-		err = writeOldMapper(out, mapper)
-		if err != nil {
-			return
+	for i, oldMapper := range reg.Mappers() {
+		node := oldMapper.ToType.BuildMapper(mapper.ProviderList{
+			{Name: oldMapper.FromName, Type: oldMapper.FromType},
+		})
+		if node == nil {
+			return fmt.Errorf("failed to map from %s to %s", oldMapper.FromType.Name(), oldMapper.ToType.Name())
 		}
+
+		mpr := &ast.Mapper{
+			Signature: fmt.Sprintf(
+				"func %s(%s %s%s) %s%s",
+				oldMapper.Name,
+				oldMapper.FromName,
+				ptr("*", oldMapper.FromType.IsPointer()),
+				oldMapper.FromType.Name(),
+				ptr("*", oldMapper.ToType.IsPointer()),
+				oldMapper.ToType.Name(),
+			),
+		}
+		mpr.Result = node.Build(mpr)
+
+		pkg.Mappers[i] = mpr
 	}
 
-	return
+	return writePackage(out, pkg)
+}
+
+func ptr(prefix string, isPointer bool) string {
+	if isPointer {
+		return prefix
+	} else {
+		return ""
+	}
 }
